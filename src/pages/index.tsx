b837ee9e98461bafb33de0b8c0488e8368b26c02
Ramiro DoLz <dolz@Ramiros-MacBook-Pro.local>
2022-05-24 23:17:46 -0300

    ADD three ADD fiber ADD drei

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,12 @@ import { lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('./home'));
-const Test = lazy(() => import('./test'));
 
 export const Routing = () => {
   return (
     <Routes>
       <Route path='home' element={<Home />} />
-      <Route path='test' element={<Test />} />
+      {/* Redirect to / if user enters invalid route */}
       <Route path='*' element={<Navigate to='/' replace />} />
     </Routes>
   );
